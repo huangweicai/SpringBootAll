793eba60601db773afdf78986734663f3440bdd1
Hans.Huang 黄伟财 <Hans.Huang@infinitus-int.com>
2019-01-23 17:59:20 +0800

    SpringBoot项目搭建初始demo Signed-off-by: Hans.Huang

--- src/main/java/com/hans/springboot/SpringbootApplication.java
+++ src/main/java/com/hans/springboot/SpringbootApplication.java
@@ -1,21 +1,9 @@
 package com.hans.springboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@SpringBootApplication
 public class SpringbootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootApplication.class, args);
     }
 
-    @RequestMapping("/")
-    String index(){
-        return "Hello My Spring Boot Demo";
-    }
 }
 
