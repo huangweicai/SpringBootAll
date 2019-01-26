c01cba787f208a0087d474d7c4c9f7c3f6a8c8c9
Hans.Huang 黄伟财 <Hans.Huang@infinitus-int.com>
2019-01-26 23:24:46 +0800

    SpringBoot+Jpa+MySql 常用配置及请求 Signed-off-by: Hans.Huang

--- 02SpringBootJpaMysql/src/main/java/com/springboot/basic_configuration/controller/MailController.java
+++ 02SpringBootJpaMysql/src/main/java/com/springboot/basic_configuration/controller/MailController.java
@@ -9,6 +9,12 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+//组合注解(RequestMapping的变形)
+//@GetMapping = @RequestMapping(method = RequestMethod.GET)
+//@PostMapping = @RequestMapping(method = RequestMethod.POST)
+//@PutMapping = @RequestMapping(method = RequestMethod.PUT)
+//@DeleteMapping = @RequestMapping(method = RequestMethod.DELETE)
+
 @RestController
 @RequestMapping("/02")
 public class MailController {
@@ -23,27 +29,53 @@ public class MailController {
         return holidayRepository.findAll();
     }
 
-    //获取url路径上的参数
-    //localhost：8080/hello/100/hello
+    //get请求，获取url路径上的参数
+    //localhost：8080/test/11/hans
     //id:81 name:hello
-    @RequestMapping(value = "/hello/{id}/{name}", method = RequestMethod.GET)
+    @RequestMapping(value = "/test/{id}/{name}", method = RequestMethod.GET)
     public String sayHello(@PathVariable("id") Integer id, @PathVariable("name") String name) {
         return "id:" + id + " name:" + name;
     }
 
-    //获取url请求参数的值
-    //localhost:8080/hello?id=1000
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    //get请求，获取url请求参数的值 @RequestParam
+    //localhost:8080/test?id=99
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
     public String sayHello(@RequestParam Integer id) {
         return "id:" + id;
     }
 
-    @RequestMapping(value = "/hello2", method = RequestMethod.GET)
+    //get请求，获取url请求参数的值，增加参数映射，默认值 @RequestParam
+    @RequestMapping(value = "/test2", method = RequestMethod.GET)
     //required=false 表示url中可以无id参数，此时就使用默认参数
     public String sayHello2(@RequestParam(value = "id", required = false, defaultValue = "1") Integer id) {
         return "id:" + id;
     }
 
+    ////////////////////////////////////////
 
+    //post请求
+    //表单参数
+    @RequestMapping(value= "/getMessage", method = RequestMethod.POST)
+    public String getMessage(int code, String message)  {
+        return "success";
+    }
 
+    //post请求
+    //json raw参数
+    @PostMapping(value= "/getMessageBody")
+    public String getMessagePost(@RequestBody HolidayEntity bean)  {
+        return "success";
+    }
+
+    //匹配参数
+    //password如果匹配对，@RequestParam不写都ok
+    public void login(@RequestParam("account") String name, @RequestParam String password) {
+        System.out.println(name + ":" + password);
+    }
+
+    //@RequestHeader注解用来将请求头的内容绑定到方法参数上。
+    @PostMapping(value = "login")
+    public void login2(@RequestHeader("access_token") String accessToken,@RequestParam String name) {
+        System.out.println("accessToken:" + accessToken);
+    }
 }
